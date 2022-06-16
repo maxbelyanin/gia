d79a379a1405493d512936d982307e800567e1a8
Max Belyanin <maxbelyanin@gmail.com>
2022-06-16 03:51:18 +0300

    fixed to localhost

--- zabbix/Doc/Administration/Scripts/avs.stream.pull.off.sh
+++ zabbix/Doc/Administration/Scripts/avs.stream.pull.off.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 stream_name=$(echo $jsontags | jq '.[] | select(.tag=="avs_stream_name") | .value')
-echo "'["$stream_name"]'" |  xargs curl -X POST -H "Content-Type: application/json" --url "http://monitoring:Vjybnjhbyu@10.103.120.66/monitoring/rec/del" -d
-echo "'["$stream_name"]'" |  xargs curl -X POST -H "Content-Type: application/json" --url "http://monitoring:Vjybnjhbyu@10.103.120.66/monitoring/pull/del" -d
+echo "'["$stream_name"]'" |  xargs curl -X POST -H "Content-Type: application/json" --url "http://monitoring:Vjybnjhbyu@localhost/monitoring/rec/del" -d
+echo "'["$stream_name"]'" |  xargs curl -X POST -H "Content-Type: application/json" --url "http://monitoring:Vjybnjhbyu@localhost/monitoring/pull/del" -d
--- zabbix/Doc/Administration/Scripts/avs.stream.pull.on.sh
+++ zabbix/Doc/Administration/Scripts/avs.stream.pull.on.sh
@@ -5,4 +5,4 @@ stream_name=$(echo $jsontags | jq '.[] | select(.tag=="avs_stream_name") | .valu
 stream_title=$(echo $jsontags | jq '.[] | select(.tag=="avs_stream_title") | .value')
 stream_comment=$(echo $jsontags | jq '.[] | select(.tag=="avs_stream_comment") | .value')
 stream_ovz=$(echo $jsontags | jq '.[] | select(.tag=="avs_stream_ovz") | .value == "true"')
-echo "'["{\"uri\": $stream_uri, \"streamName\": $stream_name, \"title\": $stream_title, \"comment\": $stream_comment, \"OVZ\": $stream_ovz}"]'" |  xargs curl -X PUT -H "Content-Type: application/json" --url "http://monitoring:Vjybnjhbyu@10.103.120.66/monitoring/pull" -d
+echo "'["{\"uri\": $stream_uri, \"streamName\": $stream_name, \"title\": $stream_title, \"comment\": $stream_comment, \"OVZ\": $stream_ovz}"]'" |  xargs curl -X PUT -H "Content-Type: application/json" --url "http://monitoring:Vjybnjhbyu@localhost/monitoring/pull" -d
